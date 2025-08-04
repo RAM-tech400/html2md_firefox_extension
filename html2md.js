65ada20a287490f9aaca7c605fecb6a37e56bbfc
Reza Alimohammadi <newram098@gmail.com>
2025-08-04 14:34:29 +0330

    Getting all elements that can to be convert to the markdown.

--- html2md.js
+++ html2md.js
@@ -1,4 +1,15 @@
 document.body.style.border = "5px solid red";
 
+let markdown = "";
+
+const allElements = document.body.querySelectorAll(
+    "h1, h2, h3, h4, h5, h6, p, ul, ol, span"
+);
+
+for(const element of allElements) {
+    console.log(element);
+}
+
+console.log(markdown);
 console.log(document.title);
 
